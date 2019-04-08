aafae2379728c5d1334e63f9736e8a7f88e97315
ravfrancis <48730171+ravfrancis@users.noreply.github.com>
2019-04-08 17:10:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+
+Designer: Ravel
+
+Main JavaScript for Total Dash
+
+*/
+
+{
+<script type= "text/javascript">
+    $(document).ready(function(){
+        $("#fas fa-plus-circle").click(function () {
+             $("#app").toggle();
+        });
+    });
+</script>
+}
+
+
